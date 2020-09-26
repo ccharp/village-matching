6cda46932ca554d1192e6e7fccbb57cb5c17f606
ccharpen <ccharpen@pm.me>
2020-09-25 22:10:54 -0700

    Full implementation--no tests

--- api.py
+++ api.py
@@ -1,15 +1,16 @@
 import requests
 import json
 
-def getApiKey():
+def get_api_key():
     with open('apikey.txt', 'r') as file:
         return file.read()
     
-def getUserPreferences():
-    url = 'https://api.airtable.com/v0/appWmmEdG1k8ceP7j/People'
-    j = requests.get(url, headers={'Authorization': 'Bearer ' + getApiKey()}).json()
+url_base = 'https://api.airtable.com/v0/appWmmEdG1k8ceP7j/'
+auth_key= 'Bearer ' + get_api_key()
 
-    print(json.dumps(j, indent=2))
+def get_user_preferences():
+    j = requests.get(url_base + 'People', headers={'Authorization': auth_key}).json()
+    #print(json.dumps(j, indent=2))
     records = j['records']
 
     idToName = {}
@@ -27,4 +28,43 @@ def getUserPreferences():
 
     return preferences
 
-print(getUserPreferences())
+def chunk_list(l, num_chunks): 
+    # looping till length l 
+    for i in range(0, len(l), num_chunks):  
+        yield l[i:i + num_chunks] 
+
+def upload_matches(matches):
+    chunked_matches = chunk_list(matches, len(matches)//10 + 1)
+    for chunked_match in chunked_matches:
+        # Wrap matches in the required fields for AirTable
+        output = {
+            'records': [{'fields': x} for x in chunked_match]
+        }
+        #print("poop my pants")
+        #print(json.dumps(output, indent=2))
+        resp = requests.post(url_base + 'Session%20Output', json=output, headers={
+            'Authorization': auth_key,
+            'Content-type': 'application/json'
+            })
+
+        print(resp.content)
+    
+
+""" Example output json
+'{
+  "records": [
+    {
+      "fields": {
+        "Person 1": [
+          "rec9khZKGdII64txx"
+        ],
+        "Person 2": [
+          "recsMmyMjmlEXDM5l"
+        ],
+        "Session": "A",
+        "Room Assignment": "Room 1"
+      }
+    },
+"""
+
+# print(get_user_preferences())
--- matcher.py
+++ matcher.py
@@ -1,19 +1,16 @@
+import copy
+import random
+import json
+
+import api
+
+
 # TODO: get data from airtable
 #   TODO: marshal data to internal format
 #   TODO: verify data integrity: 
 #       - each user listed as another user"s preference must have its own lsit of preferences
 #       - the correct number of preferences is given
 #       - the number of rounds is defined 
-
-### Dummy data
-preferences = { "A" : ["Z", "B"], 
-                "B" : ["Z", "C"], 
-                "C" : ["A", "B"], 
-                #"X" : ["A", "B"], 
-                #"Y" : ["A", "B"], 
-                "Z" : ["A", "B"]}
-
-rounds = 2
 
 
 # TODO: Actually do the match-making
@@ -32,18 +29,15 @@ rounds = 2
 # Start with
 #for p in pref_list:
 
-# Returns tuple of (number_of_matches (weight), pairings e.g. ("A", "B"))
-def gen_pairings(preferences, match_history): # e.g. { "A" : {"B", "C"}}
+#ll_ Returns tuple of (number_of_matches (weight), pairings e.g. ("A", "B"))
+def gen_round_pairings(users, preferences, match_history): # e.g. { "A" : {"B", "C"}}
     unmatched_users = set(preferences.keys())
-    all_users = preferences.keys()
-
-    # randomize unmatched_keys?
 
     pairings = [];
     num_matches = 0;
 
     # Find some actual pairings
-    for user in all_users:
+    for user in preferences.keys():
         if user not in unmatched_users:
             continue
 
@@ -51,16 +45,45 @@ def gen_pairings(preferences, match_history): # e.g. { "A" : {"B", "C"}}
             if maybe_match not in unmatched_users:
                 continue
             if(user in preferences[maybe_match] and have_never_matched(user, maybe_match, match_history)):
-                record_pairing(user, maybe_match, match_history)
                 unmatched_users.remove(user)
+
+                # TODO: Account here for person that preferred themself
                 unmatched_users.remove(maybe_match)
+                record_pairing(user, maybe_match, match_history)
                 pairings.append((user, maybe_match))
                 num_matches += 1
                 break;
 
-    # Pair the unmatched users
-    # TODO
+    # TODO look for half matches
+    unmatched_for_iterating = copy.deepcopy(unmatched_users)
+    for user1 in unmatched_for_iterating:
+        if user1 not in unmatched_users:
+            continue;
+        
+        for user2 in unmatched_users:
+            if user2 in preferences[user1] and have_never_matched(user1, user2, match_history):
+                unmatched_users.remove(user1)
+                unmatched_users.remove(user2)
+                record_pairing(user1, user2, match_history)
+                pairings.append((user1, user2))
+                num_matches += 0.5
+                break;
 
+    # Pair the unmatched users
+    # !!! We assume that we'll have a low enough round:user ratio that this is possible
+    unmatched_for_iterating = copy.deepcopy(unmatched_users)
+    for user1 in unmatched_for_iterating:
+        if user1 not in unmatched_users:
+            continue;
+        
+        unmatched_users.remove(user1)
+        for user2 in unmatched_users:
+            if have_never_matched(user1, user2, match_history):
+                unmatched_users.remove(user2)
+                record_pairing(user1, user2, match_history)
+                pairings.append((user1, user2))
+                break;
+                
     return (num_matches, pairings)
 
 def have_never_matched(u1, u2, match_history):
@@ -88,6 +111,72 @@ def record_pairing(u1, u2, match_history):
 
 # TODO: Submit results back to airtable 
 
+def generate_matches(random_attempts, game_rounds, preferences):
+    max_weight = 0
+    max_pairings_set = []
 
-pairings = gen_pairings(preferences, {})
-print(pairings)
+    for _ in range(random_attempts):
+        curr_pairings_set = []
+        curr_weight = 0
+        match_history = {}
+
+        should_skip = True
+
+        for _ in range(game_rounds):
+            all_users = random.sample(preferences.keys(), len(preferences))
+            round_pairings = gen_round_pairings(all_users, preferences, match_history) 
+            curr_weight += round_pairings[0]
+            curr_pairings_set.append(round_pairings[1])
+            print(json.dumps(round_pairings, indent=2))
+            # print(json.dumps(match_history, indent=2))
+            #print("END ROUND")
+            if len(round_pairings) < (len(preferences)//2 * game_rounds):
+                should_skip = True
+                #break 
+
+        if should_skip:
+            print('would have skipped')
+            #continue
+           
+        if curr_weight > max_weight:
+            max_weight = curr_weight
+            max_pairings_set = curr_pairings_set
+        #print("END ATTEMPT\n")
+
+    return max_pairings_set
+
+def add_game_data(matches):
+    output = [] 
+    round_number = 1
+    for round in matches:
+        room_number = 1
+        for (u1, u2) in round:
+            match_details = {
+                'Person 1': [u1],
+                'Person 2': [u2],
+                'Session': str(round_number),
+                'Room Assignment': 'Room ' + str(room_number)
+            }
+            room_number += 1
+            output.append(match_details)
+        round_number += 1
+    return output
+
+### Dummy data
+preferences_dummy = { "A" : ["Z", "B"], 
+                "B" : ["Z", "C"], 
+                "C" : ["Y", "B"], 
+                "X" : ["A", "B"], 
+                "Y" : ["A", "C"], 
+                "Z" : ["A", "B"]}
+
+#### MAIN STUFF
+preferences = api.get_user_preferences()
+matches = generate_matches(random_attempts=200, game_rounds=3, preferences=preferences)
+complete_output = add_game_data(matches)
+
+# json stuff for easy pretty printing
+#print(json.dumps(complete_output, indent=2))
+
+api.upload_matches(complete_output)
+
